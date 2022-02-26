b2b6f17cf8fcf8112e53a8f45bffe7e54e4a2e81
anamerasul <anamerasul@gmail.com>
2022-02-26 18:00:17 +0600

    Keys, values, entries, delete, seal, freeze~

---
+++ propertise.js
@@ -0,0 +1,30 @@
+const bottle ={
+        color:'yellow',
+        hold:'water',
+        price:50,
+        isCleaned:true,
+};
+// getting all properties
+const keys =Object.keys(bottle);
+console.log(keys);
+
+// getiing all values
+
+const values=Object.values(bottle);
+console.log(values);
+
+// entries
+
+const pairs =Object.entries(bottle);
+console.log(pairs);
+
+// Object.seal(bottle);
+
+Object.freeze(bottle)
+
+bottle.price=100;
+bottle.height=16;
+
+delete bottle.isCleaned;
+
+console.log(bottle)
